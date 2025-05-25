822f99c757858698283ad59703d8261f160f64df
Fede108 <federico.cechich@mi.unc.edu.ar>
2025-05-25 17:02:25 -0300

    impresion del nombre del equipo

--- kernel_module/modulo.c
+++ kernel_module/modulo.c
@@ -1,13 +1,13 @@
 #include <linux/module.h>	/* Requerido por todos los módulos */
 #include <linux/kernel.h>	/* Definición de KERN_INFO */
 MODULE_LICENSE("GPL"); 	/*  Licencia del modulo */
-MODULE_DESCRIPTION("Primer modulo ejemplo");
-MODULE_AUTHOR("Catedra de SdeC");
+MODULE_DESCRIPTION("Primer Modulo");
+MODULE_AUTHOR("Los Debuggeadores");
 
 /* Función que se invoca cuando se carga el módulo en el kernel */
 int modulo_lin_init(void)
 {
-	printk(KERN_INFO "Modulo cargado en el kernel.\n");
+	printk(KERN_INFO "Los Debuggeadores.\n");
 
 	/* Devolver 0 para indicar una carga correcta del módulo */
 	return 0;
@@ -16,7 +16,7 @@ int modulo_lin_init(void)
 /* Función que se invoca cuando se descarga el módulo del kernel */
 void modulo_lin_clean(void)
 {
-	printk(KERN_INFO "Modulo descargado del kernel.\n");
+	printk(KERN_INFO "Los Debuggeadores.\n");
 }
 
 /* Declaración de funciones init y exit */
