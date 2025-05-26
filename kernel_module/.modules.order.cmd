6a4fd44deb07b720d1d6d9323b68f60a5cc0816a
Fede108 <federico.cechich@mi.unc.edu.ar>
2025-05-26 09:54:09 -0300

    txt con modulos cargados en mi pc

--- kernel_module/.modules.order.cmd
+++
@@ -1,1 +0,0 @@
-savedcmd_/home/ignafede/Documents/Federico/IngComputacion/2025/SistComputacion/SdC_Practico-4/kernel_module/modules.order := {   echo /home/ignafede/Documents/Federico/IngComputacion/2025/SistComputacion/SdC_Practico-4/kernel_module/modulo.o; :; } > /home/ignafede/Documents/Federico/IngComputacion/2025/SistComputacion/SdC_Practico-4/kernel_module/modules.order
--- kernel_module/modules.order
+++
@@ -1,1 +0,0 @@
-/home/ignafede/Documents/Federico/IngComputacion/2025/SistComputacion/SdC_Practico-4/kernel_module/modulo.o
